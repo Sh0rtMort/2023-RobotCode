22c37ea16e11d10805cabc083064b59a50bcfe4f
Ky Duyen <98281051+daonguyenk06@users.noreply.github.com>
2023-03-02 09:44:31 -0600

    changed controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -42,14 +42,15 @@ public class RobotContainer {
 
   // Creates button mappings on the controller
   private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value); // Y button on the controller to zero the gyro
-  private final JoystickButton armUpAndOut = new JoystickButton(arm, XboxController.Axis.kLeftY.value); // Arm up and out
-  private final JoystickButton armDownAndOut = new JoystickButton(arm, -XboxController.Axis.kLeftY.value); // Arm down and out
-  private final JoystickButton armStore = new JoystickButton(arm, XboxController.Button.kX.value); // Default position
-  private final JoystickButton armMiddle = new JoystickButton(arm, XboxController.Button.kLeftStick.value); // Place object in middle row
+
+  private final JoystickButton armUpAndOut = new JoystickButton(arm, XboxController.Button.kY.value); // Arm up and out
+  private final JoystickButton armDownAndOut = new JoystickButton(arm, XboxController.Button.kA.value); // Arm down and out
+  private final JoystickButton armStore = new JoystickButton(arm, XboxController.Button.kB.value); // Default position
+  private final JoystickButton armMiddle = new JoystickButton(arm, XboxController.Button.kX.value); // Place object in middle row
   private final JoystickButton motorRelease = new JoystickButton(arm, XboxController.Button.kRightStick.value); //Arm free fall
-  private final JoystickButton zeroArmEncoders = new JoystickButton(arm, XboxController.Button.kRightBumper.value); 
-  private final JoystickButton gripperOpen = new JoystickButton(arm, XboxController.Axis.kRightX.value); //Opens claw
-  private final JoystickButton gripperClose = new JoystickButton(arm, -XboxController.Axis.kRightX.value); //Close claw
+  private final JoystickButton zeroArmEncoders = new JoystickButton(arm, XboxController.Button.kLeftStick.value); 
+  private final JoystickButton gripperOpen = new JoystickButton(arm, XboxController.Button.kLeftBumper.value); //Opens claw
+  private final JoystickButton gripperClose = new JoystickButton(arm, XboxController.Button.kRightBumper.value); //Close claw
 
 
   // Define the Swerve subsystem as swerveSubsystem
